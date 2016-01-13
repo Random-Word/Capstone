aa362f5b6a45c5973c519ca5a3978ae9a7efff75
Ross Story <ross.i.story@gmail.com>
2016-01-13 15:14:34 -0500

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, dill
+import requests
+from creds import API_KEY
 
 app = Flask(__name__)
+
+TCIA_ADDRESS = 'https://tcia.p.mashape.com/'
+#getPatientStudy
 
 @app.route('/')
 def main():
@@ -9,6 +14,13 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    response = requests.get(TCIA_ADDRESS)
+    div=""
+    script=""
+    return render_template('gallery.html', script=script, div=div)
 
 if __name__ == '__main__':
   app.run(port=33507)
